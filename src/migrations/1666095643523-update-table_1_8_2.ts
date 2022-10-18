dae2e9fa2deed3cd59414a3179fec0e2bd09bca8
buqiyuan <1743369777@qq.com>
2022-10-18 22:45:32 +0800

    fix: add script for migration

--- src/migrations/1666095643523-update-table_1_8_2.ts
+++ src/migrations/1666095643523-update-table_1_8_2.ts
@@ -1,24 +1,58 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class updateTable1821666095643523 implements MigrationInterface {
-    name = 'updateTable1821666095643523'
+  name = 'updateTable1821666095643523';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`project_users_user\` (\`projectUuid\` int NOT NULL, \`userId\` int NOT NULL, INDEX \`IDX_6a2f9d94e82e85ba623237023b\` (\`projectUuid\`), INDEX \`IDX_f8300efd87679e1e21532be980\` (\`userId\`), PRIMARY KEY (\`projectUuid\`, \`userId\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`ALTER TABLE \`api_test_history\` ADD \`createBy\` int NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`api_test_history\` ADD \`updateBy\` int NOT NULL`);
-        await queryRunner.query(`ALTER TABLE \`project_users_user\` ADD CONSTRAINT \`FK_6a2f9d94e82e85ba623237023bd\` FOREIGN KEY (\`projectUuid\`) REFERENCES \`project\`(\`uuid\`) ON DELETE CASCADE ON UPDATE CASCADE`);
-        await queryRunner.query(`ALTER TABLE \`project_users_user\` ADD CONSTRAINT \`FK_f8300efd87679e1e21532be9808\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`project_users_user\` (\`projectUuid\` int NOT NULL, \`userId\` int NOT NULL, INDEX \`IDX_6a2f9d94e82e85ba623237023b\` (\`projectUuid\`), INDEX \`IDX_f8300efd87679e1e21532be980\` (\`userId\`), PRIMARY KEY (\`projectUuid\`, \`userId\`)) ENGINE=InnoDB`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`api_test_history\` ADD \`createBy\` int NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`api_test_history\` ADD \`updateBy\` int NOT NULL`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`project_users_user\` ADD CONSTRAINT \`FK_6a2f9d94e82e85ba623237023bd\` FOREIGN KEY (\`projectUuid\`) REFERENCES \`project\`(\`uuid\`) ON DELETE CASCADE ON UPDATE CASCADE`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`project_users_user\` ADD CONSTRAINT \`FK_f8300efd87679e1e21532be9808\` FOREIGN KEY (\`userId\`) REFERENCES \`user\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+
+    const workspaces = await queryRunner.query(
+      `SELECT * FROM workspace_users_user`,
+    );
+
+    const values = workspaces.map((item) => [item.workspaceId, item.userId]);
+    console.log('values', values);
+    if (values.length) {
+      await queryRunner.query(
+        `INSERT INTO project_users_user (projectUuid, userId) VALUES ?`,
+        [values],
+      );
     }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`project_users_user\` DROP FOREIGN KEY \`FK_f8300efd87679e1e21532be9808\``);
-        await queryRunner.query(`ALTER TABLE \`project_users_user\` DROP FOREIGN KEY \`FK_6a2f9d94e82e85ba623237023bd\``);
-        await queryRunner.query(`ALTER TABLE \`api_test_history\` DROP COLUMN \`updateBy\``);
-        await queryRunner.query(`ALTER TABLE \`api_test_history\` DROP COLUMN \`createBy\``);
-        await queryRunner.query(`DROP INDEX \`IDX_f8300efd87679e1e21532be980\` ON \`project_users_user\``);
-        await queryRunner.query(`DROP INDEX \`IDX_6a2f9d94e82e85ba623237023b\` ON \`project_users_user\``);
-        await queryRunner.query(`DROP TABLE \`project_users_user\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`project_users_user\` DROP FOREIGN KEY \`FK_f8300efd87679e1e21532be9808\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`project_users_user\` DROP FOREIGN KEY \`FK_6a2f9d94e82e85ba623237023bd\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`api_test_history\` DROP COLUMN \`updateBy\``,
+    );
+    await queryRunner.query(
+      `ALTER TABLE \`api_test_history\` DROP COLUMN \`createBy\``,
+    );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_f8300efd87679e1e21532be980\` ON \`project_users_user\``,
+    );
+    await queryRunner.query(
+      `DROP INDEX \`IDX_6a2f9d94e82e85ba623237023b\` ON \`project_users_user\``,
+    );
+    await queryRunner.query(`DROP TABLE \`project_users_user\``);
+  }
 }
